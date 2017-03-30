a52d1b55f9bdaff580d5e812dd64e55970c16196
Charles Hsieh <charles@codev.tw>
2017-03-30 12:16:07 +0800

    Refer `Emitter.emit` to rewrite finish #2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ class CodevEmitter extends Emitter {
   constructor () {
     super()
   }
+
+  finish (type, ...args) {
+    // Refer the process of `Emitter.emit` to rewrite
+
+    // CORE, call the listeners and wait them to finish
+    const listeners = this.listeners(type)
+    return Promise.all(listeners.map(l => l(...args)))
+  }
 }
 
 module.exports = CodevEmitter
